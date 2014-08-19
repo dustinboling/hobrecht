a281e5035cb12156eece5a9124ff319165b0a228
Dustin Boling <dustin@dustinboling.com>
2014-08-19 02:34:58 -0700

    move deploy hooks to deploy_hooks.yml'

--- .cloud66/shared_spree.sh
+++ .cloud66/shared_spree.sh
@@ -2,5 +2,5 @@
 
 mkdir -p $STACK_BASE/shared/spree
 chown nginx:app_writers $STACK_BASE/shared/spree
-rm -rf $STACK_PATH/public/spree
+#rm -rf $STACK_PATH/public/spree
 ln -nsf $STACK_BASE/shared/spree $STACK_PATH/public/spree
