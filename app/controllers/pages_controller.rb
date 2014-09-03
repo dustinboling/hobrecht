f3e0dc4a3a3d64a63d265566443ebce3f6dc8bab
Julie Boling <julie@dustinboling.com>
2014-09-02 23:50:18 -0700

    add static pages, enable mail for spree

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+  layout "spree/layouts/spree_application"
+
+  def bio
+  end
+
+  def contact
+  end
+end
---
+++ app/helpers/pages_helper.rb
@@ -0,0 +1,12 @@
+module PagesHelper
+  current_store.url = "www.hobrecht.com"
+
+  def title
+  end
+
+  def meta_data_tags
+  end
+
+  def canonical_tagend
+  end
+end
