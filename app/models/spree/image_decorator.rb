b12c127e6d718c13edc7d6caa49ab24c7073321d
Julie Boling <julie@dustinboling.com>
2014-09-30 18:50:50 -0700

    update Spree::Image paperclip url. Bump assets version.

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -1,3 +1,4 @@
 Spree::Image.class_eval do
   attachment_definitions[:attachment][:path] = '/spree/products/:id/:style/:basename.:extension'
+  attachment_definitions[:attachment][:url] = '//s3.amazonaws.com/hobrechtmedia/spree/products/:id/:style/:basename.:extension'
 end
