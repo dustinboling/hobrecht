cd08fcfb84a4507201054fe47cbea66dbf4b980a
Dustin Boling <dustin@dustinboling.com>
2015-01-26 02:35:59 -0800

    add imagemagick build pack for heroku

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,9 @@ module Hobrecht
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # https://guides.spreecommerce.com/developer/heroku.html
+    config.assets.initialize_on_precompile = false
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Pacific Time (US & Canada)'
